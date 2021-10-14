a668a91f57ba5bb8af3e679cffeae78054d072aa
Codr Playground <support@codr.one>
2021-10-14 05:13:49 +0000

    initial

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const app = express()
+
+// const connectDB = require("./config/db")
+// connectDB()
+
+const PORT = 3000
+app.use(express.json())
+
+app.get("/", (req,res)=>res.json("test success"))
+
+// app.use("/api/sign_in", require("./routes/signIn"))
+// app.use("/api/sign_up", require("./routes/signUp"))
+// app.use("/api/clear", require("./routes/clearState"))
+
+
+app.listen(PORT, () => console.log(`Server started at ${PORT}`))
